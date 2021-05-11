8b85a7808a68c2821867ffafe4d764d6ccf2bcc0
Ben Limmer <ben.limmer@ibotta.com>
2021-05-11 16:09:29 -0600

    show working svg loader

---
+++ src/components/Demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <button id="regular-button" @click="regularButtonCounter += 1">
+        Regular Button
+      </button>
+      <span>Regular button clicked {{ regularButtonCounter }} times</span>
+    </div>
+    <div style="margin-top: 2em">
+      <CloseIcon id="svg-button" @click="svgButtonCounter += 1" />
+      <span>SVG button clicked {{ svgButtonCounter }} times</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import CloseIcon from "../assets/close-icon.svg";
+
+export default {
+  name: "Demo",
+  components: {
+    CloseIcon
+  },
+  data: function() {
+    return { regularButtonCounter: 0, svgButtonCounter: 0 };
+  }
+};
+</script>
+
+<style scoped>
+span {
+  margin-left: 2em;
+}
+</style>
